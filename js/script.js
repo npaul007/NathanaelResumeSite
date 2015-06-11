3bdf49305aba8ffd4a62156e434fd9a812977d8d
Nathanael Paulemon <npaulemon@gmail.com>
2015-06-11 13:28:21 -0400

    Bootstrap update 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+	var contact = $('#contact');
+	var education = $("#education");
+	var skills = $('#skills');
+
+	contact.click(function(e) {
+	  $("html, body").animate({ scrollTop:375 }, "slow");
+	  return false;
+	});
+	education.click(function() {
+	  $("html, body").animate({ scrollTop:860 }, "slow");
+	  return false;
+	});
+	skills.click(function() {
+	  $("html, body").animate({ scrollTop:1024 }, "slow");
+	  return false;
+	});
 });
