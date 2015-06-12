700694831d776abda155ab6604e57a3133a17b17
Nathanael Paulemon <npaulemon@gmail.com>
2015-06-12 10:47:19 -0400

    save2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	var contact = $('#contact');
 	var education = $("#education");
 	var skills = $('#skills');
@@ -7,12 +8,15 @@ $(document).ready(function(){
 	  $("html, body").animate({ scrollTop:375 }, "slow");
 	  return false;
 	});
+
 	education.click(function() {
 	  $("html, body").animate({ scrollTop:860 }, "slow");
 	  return false;
 	});
+
 	skills.click(function() {
 	  $("html, body").animate({ scrollTop:1024 }, "slow");
 	  return false;
 	});
+
 });
