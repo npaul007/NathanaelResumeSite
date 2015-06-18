e7277b767444272565573f51e6c4c4396a4e1dd3
Nathanael Paulemon <npaulemon@gmail.com>
2015-06-18 17:42:33 -0400

    fontAwesome/newColorScheme

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ $(document).ready(function(){
 	  $("html, body").animate({ scrollTop:375 }, "slow");
 	  return false;
 	});
+
 	education.click(function() {
 	  $("html, body").animate({ scrollTop:860 }, "slow");
 	  return false;
 	});
+
 	skills.click(function() {
 	  $("html, body").animate({ scrollTop:1024 }, "slow");
 	  return false;
 	});
+	
 });
