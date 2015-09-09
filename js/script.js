6eecbb25ad7dceb6a957cb655bb7281d12a5fd7a
Nate <Nate@mac-users-MacBook-Pro-2.local>
2015-09-08 20:55:57 -0400

    design change 9

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('.fa-github').click(function(){window.location.href = 'https://github.com/npaul007'});
+	$('.fa-envelope').click(function(){window.location.href='mailto:napp22@yahoo.com';});
 	$('.resume-button').click(function(){window.location.href = 'files/Resume.docx'});
 
 	/*
