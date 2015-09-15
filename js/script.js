56702bbd2c7eca276ac8ca96073c2fae24eab71a
Nate <Nate@mac-users-MacBook-Pro-2.local>
2015-09-15 00:54:06 -0400

    design change 23

--- js/script.js
+++ js/script.js
@@ -4,29 +4,37 @@ $(document).ready(function(){
 	$('.resume-button').click(function(){window.location.href = 'files/Resume.docx'});
 	$('#pong-button').click(function(){window.location.href ='http://unipong.meteor.com'});
 
-	var aboutme = $('#caboutme-modal');
+	var aboutme = $('#aboutme-modal');
 	var skills = $('#skills-modal');
 	var contact = $('#contact-modal');
 	var projects = $('#projects-modal');
 
 	contact.click(function(e) {
-	  $("html, body").animate({ scrollTop:$("contact").offset().top }, "slow");
+	  $("html, body").animate({ scrollTop:$("#contact").offset().top }, "slow");
+	  closeModal();
 	  return false;
 	});
 
 	skills.click(function() {
-	  $("html, body").animate({ scrollTop:$("#skulls").offset().top }, "slow");
+	  $("html, body").animate({ scrollTop:$("#skills").offset().top-40 }, "slow");
+	  closeModal();
 	  return false;
 	});
 
 	projects.click(function() {
-	  $("html, body").animate({ scrollTop:$("#projects").offset().top }, "slow");
+	  $("html, body").animate({ scrollTop:$("#projects").offset().top-40 }, "slow");
+	  closeModal();
 	  return false;
 	});
 
 	aboutme.click(function() {
-	  $("html, body").animate({ scrollTop:$("#aboutme").offset().top }, "slow");
+	  $("html, body").animate({ scrollTop:$("#aboutme").offset().top-40 }, "slow");
+	  closeModal();
 	  return false;
 	});
+
+	function closeModal(){
+		$('#myModal').close();
+	}
 
 });
