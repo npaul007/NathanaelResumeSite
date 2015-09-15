bbffda9b422ed556bc3434f05467bf2d49e25ffc
Nate <npaulemon@gmail.com>
2015-09-15 14:52:02 -0400

    commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$('.fa-github').click(function(){window.location.href = 'https://github.com/npaul007'});
+	$('.fa-envelope').click(function(){window.location.href='mailto:napp22@yahoo.com';});
+	$('.resume-button').click(function(){window.location.href = 'files/Resume.docx'});
+	$('#pong-button').click(function(){window.location.href ='http://unipong.meteor.com'});
+
+	var aboutme = $('#aboutme-modal');
+	var skills = $('#skills-modal');
+	var contact = $('#contact-modal');
+	var projects = $('#projects-modal');
+
+	contact.click(function(e) {
+	  $("html, body").animate({ scrollTop:$("#contact").offset().top-45 }, "slow");
+	  closeModal();
+	  return false;
+	});
+
+	skills.click(function() {
+	  $("html, body").animate({ scrollTop:$("#skills").offset().top-45 }, "slow");
+	  closeModal();
+	  return false;
+	});
+
+	projects.click(function() {
+	  $("html, body").animate({ scrollTop:$("#projects").offset().top-45 }, "slow");
+	  closeModal();
+	  return false;
+	});
+
+	aboutme.click(function() {
+	  $("html, body").animate({ scrollTop:$("#aboutme").offset().top-45 }, "slow");
+	  closeModal();
+	  return false;
+	});
+
+	function closeModal(){
+		$('#myModal').close();
+	}
+
+});
