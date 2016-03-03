a75a847dd5a0a8d3db8ef56ba8bc87b08a94d532
Nate <npaulemon@gmail.com>
2016-03-02 19:09:40 -0500

    animate css removed

--- js/script.js
+++ js/script.js
@@ -1,44 +1,4 @@
 $(document).ready(function(){
-
-	/* contact.click(function(e) {
-	  $("html, body").animate({ scrollTop:$("#contact").offset().top-45 }, "slow");
-	  closeModal();
-	  return false;
-	}); */
 	
-	var windowWidth = $(window).width();
-
-	if (windowWidth > 1000) {
-		$('#myPicture').addClass('animated');
-		$('#myName').addClass('animated');
-		$('#myQuote').addClass('animated');
-	}
-
-	var sectionsAnimated = [
-		'#aboutMe',
-		'#educationExperience'
-	];
-
-	for(var j = 0; j < sectionsAnimated.length; j++)
-		$(sectionsAnimated[j]+' *').hide();
-
-	$(window).scroll(function(){
-		for(var i = 0; i < sectionsAnimated.length; i++){
-			if(ifSeenOnScreen(sectionsAnimated[i])){
-				$(sectionsAnimated[i]+' *').show();
-				$(sectionsAnimated[i]+" *").addClass('animated');
-			}
-		}
-	});
-
 });
 
-function ifSeenOnScreen(el) {
-   var docViewTop = $(window).scrollTop(),
-       docViewBottom = docViewTop + $(window).height(),
-       elemTop = $(el).offset().top,
-       elemBottom = elemTop + $(el).height();
- 
-   return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
-}
- 
