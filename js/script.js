4b6f0b47d6c7e21e6538fe5571fb35af89fbf321
Nate <npaulemon@gmail.com>
2016-03-20 21:26:54 -0400

    side menu added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
-	
+	$('#sideBarMenu').hide();
+
+	$('#menuButton').click(function(){
+		$('#sideBarMenu').toggle('slide');
+		menuButtonIconStatus();
+	});
+
+	$('#sideBarMenuContents').click(function(){
+		$('#sideBarMenu').toggle('slide');
+		menuButtonIconStatus();
+	});
 });
 
+function menuButtonIconStatus(){
+	if($('#menuButton').hasClass('fa-bars')){
+		$('#menuButton').removeClass('fa-bars');
+		$('#menuButton').addClass('fa-times');
+	}
+
+	else{
+		$('#menuButton').removeClass('fa-times');
+		$('#menuButton').addClass('fa-bars');
+	}
+}
