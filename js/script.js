ec9c86de276c41749bb1fb55e6dd84f133276621
Nathanael <npaulemon@gmail.com>
2017-04-13 18:59:07 -0400

    style changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,67 @@
 $(document).ready(function(){
-	$('#sideBarMenu').hide();
+	//hide menu on pageload
+	hideMenu();
 
-	$('#menuButton').click(function(){
+	$('#menuButton, #sideBarMenu>#sideBarMenuContents>a').click(function(){
 		menuButtonIconStatus();
 	});
 
-	$('#sideBarMenu').click(function(){
-		menuButtonIconStatus();
+	// if menu is open and you click outside of it, close it
+	$('html').click(function(e){
+		if( e.target.id != 'menuButton' && 
+			e.target.id != 'sideBarMenu' &&
+			e.target.className != '' &&
+			e.target.id != 'sideBarMenu>#sideBarMenuContents' ||
+			e.target.className != 'sidebar-links' &&
+			menuIsOpen() )
+
+			hideMenu();
+
 	});
 });
 
-function menuButtonIconStatus(){
-	if($('#menuButton').hasClass('fa-bars')){
-		$('#menuButton').removeClass('fa-bars');
-		$('#menuButton').addClass('fa-times');
-		$('#sideBarMenu').show();
-	}
+// loading screen logic
+window.addEventListener("load",function(){
+	setTimeout(function(){
+		$('#loadingScreen').addClass('hidden');
+		$('#wrapper').removeClass('hidden');
+	},1000);
+});
 
-	else{
-		$('#menuButton').removeClass('fa-times');
-		$('#menuButton').addClass('fa-bars');
-		$('#sideBarMenu').hide();
+// check to see if menu is open or not based on icon
+function menuButtonIconStatus(){
+	if(menuIsOpen()){
+		showMenu();
+	}else{
+		hideMenu();
 	}
+}
+
+function showMenu(){
+	// if closed show x
+	$('#menuButton').removeClass('fa-bars');
+	$('#menuButton').addClass('fa-times');
+
+	$('#sideBarMenu').removeClass('slideOutRight');
+
+	$('#sideBarMenu').show();
+
+	$('#sideBarMenu').addClass('slideInRight animated');
+}
+
+function hideMenu(){
+	// if open show bars
+	$('#menuButton').removeClass('fa-times');
+	$('#menuButton').addClass('fa-bars');
+
+	$('#sideBarMenu').addClass('slideOutRight');
+
+	setTimeout(function(){
+		$('#sideBarMenu').fadeOut();
+	},500);
+}
+
+// if menu is open return true
+function menuIsOpen(){
+	return $('#menuButton').hasClass('fa-bars');
 }
