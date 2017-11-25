b2eb62fa4ed072de4f00015b1fe52aa66b08251a
Nate <npaulemon@gmail.com>
2017-11-24 19:57:44 -0500

    added more relevant courses etc

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-$(document).ready(function(){
-	$('#menuButton, #sideBarMenu>#sideBarMenuContents>a').click(function(){menuButtonIconStatus()});
-	$('.excluded').click(function(){hideMenu()});
-});
-
-// loading screen logic
-window.addEventListener("load",function(){
-	hideMenu();
-
-	setTimeout(function(){
-		$('#loadingScreen').addClass('hidden');
-		$('#wrapper').removeClass('hidden');
-		$('body').addClass('fadeIn animated');
-	},1000);
-});
-
 // check to see if menu is open or not based on icon
 function menuButtonIconStatus(){
 	(menuIsOpen()) ? showMenu() : hideMenu();
@@ -31,12 +15,26 @@ function hideMenu(){
 
 	$('#sideBarMenu').addClass('slideOutRight');
 
-	setTimeout(function(){
-		$('#sideBarMenu').fadeOut();
-	},500);
+	setTimeout(function(){$('#sideBarMenu').fadeOut()},500);
 }
 
 // if menu is open return true
 function menuIsOpen(){
 	return $('#menuButton').hasClass('fa-bars');
 }
+
+$(document).ready(function(){
+	$('#menuButton, #sideBarMenu > #sideBarMenuContents >a').click(function(){menuButtonIconStatus()});
+	$('.excluded').click(function(){hideMenu()});
+});
+
+// loading screen logic
+window.addEventListener("load",function(){
+	hideMenu();
+
+	setTimeout(function(){
+		$('#loadingScreen').addClass('hidden');
+		$('#wrapper').removeClass('hidden');
+		$('body').addClass('fadeIn animated');
+	},250);
+});
